aef2430a5bd49ae444660b432b5271331f12a247
Matha Charan <mathacharan3004@gmail.com>
2026-06-21 13:42:01 +0530

    fix: use D logo as favicon via Next.js app/favicon.ico

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,7 +37,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/jpeg" href="/logo.jpeg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
